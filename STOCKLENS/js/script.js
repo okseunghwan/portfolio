bed14796c462fb4ef9373969a9c02241da83bf3a
Ok Seung Hwan <pokd7004@naver.com>
2026-04-09 14:47:46 +0900

    stocklens 페이지 수정

---
+++ STOCKLENS/js/script.js
@@ -0,0 +1,40 @@
+$(function () {
+    $(".open_btn").click(function () {
+        $(".m_menu_wrap").css({
+            'left': '50vw',
+            'transition': 'left 0.3s'
+        })
+    })
+    $(".close_btn").click(function () {
+        $(".m_menu_wrap").css({
+            'left': '100vw',
+            'transition': 'left 0.3s'
+        })
+    })
+    $(".m_menu li a").click(function () {
+        $(".m_menu_wrap").css({
+            'left': '100vw',
+            'transition': 'left 0.3s'
+        })
+    })
+
+    // hamberger end
+
+
+
+
+
+
+
+
+    // tab
+
+    $(".board>li>a").click(function(){
+        $(this).parent().addClass("on").siblings().removeClass("on")
+    })
+    $(".market_board>li>a").click(function(){
+        $(this).parent().addClass("on").siblings().removeClass("on")
+    })
+
+
+})
